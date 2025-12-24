e6e995b668d0ed5327f1383d8006b4aab4d211df
billy-rubin <rumit211@gmail.com>
2025-12-24 14:27:11 +0700

    socks-proxy

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -5,12 +5,10 @@ import (
 	"os"
 )
 
-// Setup инициализирует логгер. Можно сделать JSON или Text формат.
 func Setup() *slog.Logger {
 	opts := &slog.HandlerOptions{
-		Level: slog.LevelDebug, // Чтобы видеть всё
+		Level: slog.LevelDebug,
 	}
-	// Используем TextHandler для читаемости в консоли (или JSONHandler для продакшена)
 	handler := slog.NewTextHandler(os.Stdout, opts)
 	return slog.New(handler)
 }
